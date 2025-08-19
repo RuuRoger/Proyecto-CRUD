276cef1a4cb610317c8e7ec57661314745fc2619
Roger Lozano <lozanofierroruu@gmail.com>
2025-08-19 07:41:58 +0200

    Se ha especificado el nombre real de la tabla y la llave primaria

--- Models/ContactoModel.cs
+++ Models/ContactoModel.cs
@@ -1,18 +1,20 @@
-using System;
 using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace Proyecto_CRUD.Models;
 
+[Table("CONTACTO")]
 public class ContactoModel
 {
+    [Key]
     public int IdContacto { get; set; }
 
     [Required]
     public string Nombre { get; set; } = string.Empty;
 
     [Required]
-        public string Telefono { get; set; } = string.Empty;
+    public string Telefono { get; set; } = string.Empty;
 
     [Required]
     public string Email { get; set; } = string.Empty;
